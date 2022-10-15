1e68dbfce2bc81b083e2331db0d14c6d32733795
tungnx <nxtung95@gmail.com>
2022-10-15 15:23:07 +0700

    Up register & login

--- src/main/java/vn/ngong/request/LoginRequest.java
+++ src/main/java/vn/ngong/request/LoginRequest.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
 
 @Getter
 public class LoginRequest implements Serializable {
-	@Schema(required = true, example = "User: user_ngong@gmail.com\n Admin: admin_ngong@gmail.com")
+	@Schema(required = true, description = "Tài khoản user: user_ngong@gmail.com, Tài khoản admin: admin_ngong@gmail.com", example = "user_ngong@gmail.com")
 	private String username;
 	@Schema(required = true, example = "1234")
 	private String password;
