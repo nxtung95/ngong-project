057dcb938037ed9ef77c4d224d0eb23c14c3c102
tungnx <nxtung95@gmail.com>
2022-10-17 00:08:04 +0700

    Fix cors

--- src/main/java/vn/ngong/config/WebSecurityConfig.java
+++ src/main/java/vn/ngong/config/WebSecurityConfig.java
@@ -47,9 +47,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		// We don't need CSRF for this example
-		httpSecurity.csrf().disable()
+		httpSecurity.cors().and().csrf().disable()
 				// dont authenticate this particular request
-				.authorizeRequests().antMatchers("/login", "/health", "/register", "/swagger-ui/**", "/v3/api-docs/**").permitAll().
+				.authorizeRequests()
+				.antMatchers("/login", "/health", "/register", "/swagger-ui/**", "/v3/api-docs/**").permitAll().
 				// all other requests need to be authenticated
 						anyRequest().authenticated().and().
 				// make sure we use stateless session; session won't be used to
