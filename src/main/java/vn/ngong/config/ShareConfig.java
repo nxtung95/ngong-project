68fa404e55ad79f0e622ccda10d7187ec13c39ab
tungnx <nxtung95@gmail.com>
2022-10-20 00:09:40 +0700

    up

--- src/main/java/vn/ngong/config/ShareConfig.java
+++ src/main/java/vn/ngong/config/ShareConfig.java
@@ -21,7 +21,7 @@ public class ShareConfig {
 	@Value("${kiotviet.token.client-id:b0c70023-7e2d-452b-a06a-dd9820931964}")
 	private String kiotVietClientId;
 
-	@Value("${kiotviet.token.client-id:41F4E5A10F57D466F6614A4A584F83D1FC6B533B}")
+	@Value("${kiotviet.token.client-secret:41F4E5A10F57D466F6614A4A584F83D1FC6B533B}")
 	private String kiotVietClientSecret;
 
 	@Value("${kiotviet.token.detail-product.url:https://public.kiotapi.com/products/code}")
