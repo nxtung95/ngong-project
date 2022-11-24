8a8de65850fcdbd7f1caef3d3f95c42f09e2a3d4
hoangbc <hoangbc@edsolabs.com>
2022-11-24 21:17:45 +0700

    - update ship

--- src/main/java/vn/ngong/repository/ShipRepository.java
+++ src/main/java/vn/ngong/repository/ShipRepository.java
@@ -23,13 +23,13 @@ public class ShipRepository {
         try {
             Query query = entityManager.createNativeQuery("SELECT s.id, s.price" +
                     " FROM ships s " +
-                    " INNER JOIN ship_areas a ON a.id = s.ship_area_id" +
+                    " INNER JOIN ship_areas a ON a.code = s.ship_area_code" +
                     " WHERE s.`status` = 1" +
                     " AND a.`status` = 1" +
                     " AND a.city_code = " + cityCode +
-                    " AND IFNULL(a.district_code, '') = '' OR a.district_code = " + districtCode +
-                    " AND ((s.start_kg = 0 AND s.end_kg = 0) OR (s.end_kg = 0 AND s.start_kg <= " + weight + ") OR (" + weight + " BETWEEN s.end_kg AND s.start_kg))" +
-                    " AND ((s.start_money = 0 AND s.end_money = 0) OR (s.end_money = 0 AND s.start_money <= " + totalPrice + ") OR (" + totalPrice + " BETWEEN s.end_money AND s.start_money))" +
+                    " AND (IFNULL(a.district_code, '') = '' OR a.district_code = " + districtCode + ")" +
+                    " AND ((s.start_kg = 0 AND s.end_kg = 0) OR (s.end_kg = 0 AND s.start_kg <= " + weight + ") OR (" + weight + " BETWEEN s.start_kg AND s.end_kg))" +
+                    " AND ((s.start_money = 0 AND s.end_money = 0) OR (s.end_money = 0 AND s.start_money <= " + totalPrice + ") OR (" + totalPrice + " BETWEEN s.start_money AND s.end_money))" +
                     " LIMIT 1");
             List<Object[]> objects = query.getResultList();
             List<Ship> ships = new ArrayList<>();
