2ce770fac9fd5029f74d63938d1fc4a15cd52fb9
tungnx <nxtung95@gmail.com>
2022-12-03 19:37:35 +0700

    load cache city when deploy app

---
+++ src/main/java/vn/ngong/config/InitBeanApp.java
@@ -0,0 +1,20 @@
+package vn.ngong.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import vn.ngong.cache.LocalCacheConfig;
+
+@Slf4j
+@Component
+public class InitBeanApp implements InitializingBean {
+	@Autowired
+	private LocalCacheConfig localCacheConfig;
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		localCacheConfig.loadCityAgentCTVList();
+	}
+
+}
