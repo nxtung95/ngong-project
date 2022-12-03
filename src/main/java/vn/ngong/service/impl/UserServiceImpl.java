4931d112ed222965888772c22a3133221e968751
tungnx <nxtung95@gmail.com>
2022-12-03 08:43:50 +0700

    Fix bug user

--- src/main/java/vn/ngong/service/impl/UserServiceImpl.java
+++ src/main/java/vn/ngong/service/impl/UserServiceImpl.java
@@ -78,6 +78,18 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
+	public boolean checkExistByEmail(String mail, int userId) {
+		User user = userRepository.findByEmail(mail).orElse(null);
+		if (user == null) {
+			return true;
+		}
+		if (user.getId() == userId) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
 	public boolean update(User user) {
 		try {
 			User userByPhone = userRepository.findByPhone(user.getPhone()).orElse(null);
